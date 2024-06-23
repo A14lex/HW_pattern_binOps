c7399b71ec1ad9b3a73fb28e5bc802a9a4d5d257
Alex330790 <ajkul@mail.ru>
2024-06-23 11:22:04 +0300

    start

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.sum("1100", "0011"));
+        System.out.println(binOps.mult("1100", "0011"));
+    }
+}
